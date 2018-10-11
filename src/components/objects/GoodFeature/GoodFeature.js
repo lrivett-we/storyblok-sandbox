554013135211dd1ab5d015404948e6af2a3c72cf
lrivett-we <luke.rivett@we.org>
2018-10-11 16:11:44 -0400

    Changed format of deprecation to use a generated component as the deprecated component. Deleted components will be removed from stories. Added goodFeature component and weirdPage content type, which are currently not available from components.js.

---
+++ src/components/objects/GoodFeature/GoodFeature.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GoodFeature = (props) => (
+  <div className="col-4">
+    <h2 style={{color: "green"}}>{props.name}</h2>
+  </div>
+)
+
+export default GoodFeature
